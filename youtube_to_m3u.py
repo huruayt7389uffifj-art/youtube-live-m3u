4b68eff6230c42d096c8e5f71915891b8c8e7fe7
huruayt7389uffifj-art <huruayt7389uffifj@gmail.com>
2026-02-09 07:10:30 +0200

    Update youtube_to_m3u.py

--- youtube_to_m3u.py
+++ youtube_to_m3u.py
@@ -14,8 +14,8 @@ channels = [
 with open("playlist.m3u", "w", encoding="utf-8") as f:
     f.write("#EXTM3U\n")
     for name, vid_id in channels:
-        # سيرفر yewtu.be هو الأكثر استقراراً الآن في 2026
+        # سيرفر yewtu.be هو الأكثر موثوقية لتخطي حماية يوتيوب حالياً
         m3u8_link = f"https://yewtu.be/latest_version?id={vid_id}&itag=96"
         f.write(f'#EXTINF:-1, {name}\n{m3u8_link}\n')
 
-print("Done!")
+print("Update successful.")
